90e66ba0c2d511b04ee5dfaf12e1c34ea7631d2f
freedfelipe <freedfelipe@gmail.com>
2014-08-26 11:43:45 -0300

    CRUD de vendedores

--- application/views/dashboard/template/sidebar.php
+++ application/views/dashboard/template/sidebar.php
@@ -26,5 +26,18 @@
 				</li>
 			</ul>
 		</li>
+		<li>
+			<a href="javascript:" class="dropdown-toggle"> <i class="icon-group"></i> <span>Vendedores</span>
+				<i class="icon-chevron-down"></i>
+			</a>
+			<ul class="submenu">
+				<li>
+					<a href="<?=site_url('seller/index');?>">Listagem</a>
+				</li>
+				<li>
+					<a href="<?=site_url('seller/create');?>">Adicionar</a>
+				</li>
+			</ul>
+		</li>
 	</ul>
 </div>
